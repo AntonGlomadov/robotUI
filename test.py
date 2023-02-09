0912d411346625f16826ae959d0d96a025514fa4
Anton <tony-Zoom-Zoom@mail.ru>
2023-02-09 15:08:05 +0200

    speed icons

--- test.py
+++ test.py
@@ -1,20 +1,33 @@
 import os
 import sys
-from PyQt5.QtCore import(QTimer)
-from PyQt5.QtWidgets import(QApplication, QPushButton)
+from PyQt5.QtCore import (QTimer)
+from PyQt5.QtWidgets import (QApplication, QPushButton)
+from PyQt5.QtCore import Qt
 
-def button_pressed(timer):
-    timer.start(100)
 
-def button_released(timer):
-    timer.stop()
+'''окно отображающее переменную i. Когда пользователь нажимает и удерживает кнопку w на клавиатуре, i увеличивается на 1 каждые 100 мс. 
+Когда пользователь отпускает кнопку, i уменьшается на 1 каждые 100 мс.'''
+
+
+class MainWindow(QPushButton):
+    def __init__(self):
+        super().__init__()
+        self.i = 0
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(100)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_W:
+            self.i += 1
+
+    def update(self):
+        self.i -= 1
+        self.setText(str(self.i))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    pb = QPushButton("Press")
-    timer = QTimer()
-    pb.pressed.connect(lambda checked = False: button_pressed(timer))
-    pb.released.connect(lambda checked = False: button_released(timer))
-    timer.timeout.connect(lambda: print('Button Pressed'))
-    pb.show()
-    app.exec_()
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
